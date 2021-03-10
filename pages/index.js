37d671874b3e6f6ee648e7be708bc31711e260ff
Rory Hughes <544628+rorhug@users.noreply.github.com>
2021-03-10 13:35:11 +0000

    nit

--- pages/index.js
+++ pages/index.js
@@ -4,78 +4,66 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Masqhaus</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          Masqhaus
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          <div
+            className="p-6 mt-6 text-left w-96"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+            <img src="/logo.jpeg" />
+          </div>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+          <div className=" w-96">
+            <span
+              className="p-6 mt-6 text-left border w-96 rounded-xl block text-gray-300"
+            >
+              <h3 className="text-2xl font-bold mb-4">NFT Drop &rarr;</h3>
+              <code className="p-3 font-mono text-xs bg-gray-100 rounded-md">
+                coming soon
+              </code>
+            </span>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+            <a
+              href="https://www.instagram.com/masqhaus"
+              target="_blank"
+              className="p-6 mt-6 text-left border w-96 rounded-xl block hover:text-blue-600 focus:text-blue-600"
+            >
+              <h3 className="text-2xl font-bold">Instagram &rarr;</h3>
+              <p className="mt-4 text-xl">
+                Follow for pieces and updates.
+              </p>
+            </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <a
+              href="mailto:freddie@masqhaus.com"
+              target="_blank"
+              className="p-6 mt-6 text-left border w-96 rounded-xl block hover:text-blue-600 focus:text-blue-600"
+            >
+              <h3 className="text-2xl font-bold">Contact &rarr;</h3>
+              <p className="mt-4 text-xl">
+                Email me for more information.
+              </p>
+              <p className="mt-4 text-sm">
+                freddie@masqhaus.com
+              </p>
+            </a>
+
+          </div>
+
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+      <footer className="flex items-center justify-center w-full h-16 border-t">
+        <p className="text-center text-gray-400">
+          © 2021 Masqhaus
+        </p>
       </footer>
     </div>
   )
